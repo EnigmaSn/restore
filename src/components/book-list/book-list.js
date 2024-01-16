ec3cf10b374569e73bf39b910f652fa04a417d3a
Anna Varaksina <enigmasnape@gmail.com>
2024-01-16 17:54:44 +0300

    action dispatch

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,27 +1,47 @@
-import React, { Component } from "react";
+import React, { useLayoutEffect } from "react";
 import BookListItem from "../book-list-item";
 import { connect } from "react-redux";
 
-class BookList extends Component {
-  render() {
-    const { books } = this.props;
+import { withBookstoreService } from "../hoc";
+import { booksLoaded } from "../../actions";
+import { compose } from "../../utils";
 
-    return (
-      <ul>
-        {books.map((book) => {
-          return (
-            <li key={book.id}>
-              <BookListItem book={book}></BookListItem>
-            </li>
-          );
-        })}
-      </ul>
-    );
-  }
-}
+import "./book-list.scss";
 
-const mapStateToProps = (state) => {
-  return { books: state };
+const BookList = (props) => {
+  const { books } = props;
+
+  useLayoutEffect(() => {
+    const { bookstoreService, booksLoaded } = props;
+
+    // 1. receive data
+    const data = bookstoreService.getBooks();
+    // 2. dispacth action to store
+    booksLoaded(data);
+  }, []);
+
+  return (
+    <ul>
+      {books?.map((book) => {
+        return (
+          <li key={book.id}>
+            <BookListItem book={book} />
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
-export default connect(mapStateToProps, {})(BookList);
+const mapStateToProps = (state) => {
+  return { books: state.books };
+};
+
+const mapDispatchToProps = {
+  booksLoaded,
+};
+
+export default compose(
+  withBookstoreService(),
+  connect(mapStateToProps, mapDispatchToProps)
+)(BookList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,4 @@
-const initialState = [
-  { id: 1, title: "Harry Potter and FS", author: "JKR" },
-  { id: 2, title: "Harry Potter and CS", author: "JKR" },
-];
+const initialState = [];
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {

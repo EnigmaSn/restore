28c71abcba795c43e8cc9398a819c6382fb0e355
Anna Varaksina <enigmasnape@gmail.com>
2024-01-19 22:38:48 +0300

    shoppingCardTable connect to redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,24 +2,29 @@ const initialState = {
   books: [],
   loading: true,
   error: null,
+  cartItems: [{ id: 1, name: "test", count: 3, total: 394 }],
+  orderTotal: 220,
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "FETCH_BOOKS_REQUEST":
       return {
+        ...state,
         books: [],
         loading: true,
         error: null,
       };
     case "FETCH_BOOKS_SUCCESS":
       return {
+        ...state,
         books: action.payload,
         loading: false,
         error: null,
       };
     case "FETCH_BOOKS_FAILURE":
       return {
+        ...state,
         books: [],
         loading: false,
         error: action.payload,

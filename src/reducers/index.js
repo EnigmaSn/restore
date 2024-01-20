57e2cf46cde262470710364be1cb17ab7920a345
Anna Varaksina <enigmasnape@gmail.com>
2024-01-20 20:27:23 +0300

    redux update element in array

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,6 +6,29 @@ const initialState = {
   orderTotal: 220,
 };
 
+const updateCartItems = (cartItems, item, itemIdx) => {
+  if (itemIdx === -1) {
+    return [...cartItems, item];
+  }
+
+  return [
+    ...cartItems.slice(0, itemIdx),
+    item,
+    ...cartItems.slice(itemIdx + 1),
+  ];
+};
+
+const updateCartItem = (book, item = {}) => {
+  const { id = book.id, count = 0, title = book.title, total = 0 } = item;
+
+  return {
+    id,
+    title,
+    count: count + 1,
+    total: total + book.price,
+  };
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "FETCH_BOOKS_REQUEST":
@@ -33,16 +56,16 @@ const reducer = (state = initialState, action) => {
     case "BOOK_ADDED_TO_CART":
       const bookId = action.payload;
       const book = state.books.find((book) => book.id === bookId);
-      const newItem = {
-        id: book.id,
-        name: book.title,
-        count: 1,
-        total: book.price,
-      };
+
+      const itemIdx = state.cartItems.findIndex(
+        (cartItem) => cartItem.id === bookId
+      );
+      const item = state.cartItems[itemIdx];
+      const newItem = updateCartItem(book, item);
 
       return {
         ...state,
-        cartItems: [...state.cartItems, newItem],
+        cartItems: updateCartItems(state.cartItems, newItem, itemIdx),
       };
 
     default:

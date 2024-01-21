0bf490ac685e9d41de57b0daf95bc7b241d5365a
Anna Varaksina <enigmasnape@gmail.com>
2024-01-21 20:55:50 +0300

    middleware

--- src/store.js
+++ src/store.js
@@ -1,6 +1,28 @@
 import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./reducers";
 
-const store = configureStore({ reducer });
+const logMiddleware =
+  ({ getState }) =>
+  (next) =>
+  (action) => {
+    console.log(action.type, getState());
+    return next(action);
+  };
+
+const stringMiddleware = () => (next) => (action) => {
+  if (typeof action === "string") {
+    return next({
+      type: action,
+    });
+  }
+
+  return next(action);
+};
+
+const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(stringMiddleware).concat(logMiddleware),
+});
 
 export default store;

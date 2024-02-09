b1dda5f2620640dfd1cbed487856736c4283778f
Anna Varaksina <enigmasnape@gmail.com>
2024-02-09 16:45:45 +0300

    tailwind table

--- src/components/shopping-cart-table/shopping-cart-table.js
+++ src/components/shopping-cart-table/shopping-cart-table.js
@@ -17,29 +17,62 @@ const ShoppingCartTable = ({
   const renderRow = (item, idx) => {
     const { id, title, count, total } = item;
     return (
-      <tr key={id}>
+      <tr key={id} className="border-b">
         <td>{idx + 1}</td>
         <td>{title}</td>
         <td>{count}</td>
         <td>${total}</td>
-        <td>
+        <td className="flex justify-end">
           <button
             onClick={() => onDelete(id)}
-            className="btn btn-outline-danger btn-sm float-right"
+            className="button button--danger"
           >
-            <i className="fa fa-trash" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              class="w-5 h-5"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8.75 1A2.75 2.75 0 0 0 6 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 1 0 .23 1.482l.149-.022.841 10.518A2.75 2.75 0 0 0 7.596 19h4.807a2.75 2.75 0 0 0 2.742-2.53l.841-10.52.149.023a.75.75 0 0 0 .23-1.482A41.03 41.03 0 0 0 14 4.193V3.75A2.75 2.75 0 0 0 11.25 1h-2.5ZM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4ZM8.58 7.72a.75.75 0 0 0-1.5.06l.3 7.5a.75.75 0 1 0 1.5-.06l-.3-7.5Zm4.34.06a.75.75 0 1 0-1.5-.06l-.3 7.5a.75.75 0 1 0 1.5.06l.3-7.5Z"
+                clip-rule="evenodd"
+              />
+            </svg>
           </button>
           <button
             onClick={() => onIncrease(id)}
-            className="btn btn-outline-success btn-sm float-right"
+            className="button button--success"
           >
-            <i className="fa fa-plus-circle" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              class="w-5 h-5"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4.5 2A1.5 1.5 0 0 0 3 3.5v13A1.5 1.5 0 0 0 4.5 18h11a1.5 1.5 0 0 0 1.5-1.5V7.621a1.5 1.5 0 0 0-.44-1.06l-4.12-4.122A1.5 1.5 0 0 0 11.378 2H4.5ZM10 8a.75.75 0 0 1 .75.75v1.5h1.5a.75.75 0 0 1 0 1.5h-1.5v1.5a.75.75 0 0 1-1.5 0v-1.5h-1.5a.75.75 0 0 1 0-1.5h1.5v-1.5A.75.75 0 0 1 10 8Z"
+                clip-rule="evenodd"
+              />
+            </svg>
           </button>
           <button
             onClick={() => onDecrease(id)}
-            className="btn btn-outline-warning btn-sm float-right"
+            className="button button--warning"
           >
-            <i className="fa fa-minus-circle" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              class="w-5 h-5"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4.5 2A1.5 1.5 0 0 0 3 3.5v13A1.5 1.5 0 0 0 4.5 18h11a1.5 1.5 0 0 0 1.5-1.5V7.621a1.5 1.5 0 0 0-.44-1.06l-4.12-4.122A1.5 1.5 0 0 0 11.378 2H4.5Zm7.75 9.75a.75.75 0 0 0 0-1.5h-4.5a.75.75 0 0 0 0 1.5h4.5Z"
+                clip-rule="evenodd"
+              />
+            </svg>
           </button>
         </td>
       </tr>
@@ -47,9 +80,9 @@ const ShoppingCartTable = ({
   };
 
   return (
-    <div className="shopping-cart-table">
-      <h2>Your Order</h2>
-      <table className="table">
+    <div className="mt-4">
+      <h2 className="text-2xl mb-3">Your Order</h2>
+      <table className="table- w-full">
         <thead>
           <tr>
             <th>#</th>
@@ -63,7 +96,7 @@ const ShoppingCartTable = ({
         <tbody>{items.map(renderRow)}</tbody>
       </table>
 
-      <div className="total">Total: ${total}</div>
+      <div className="total text-xl text-right m-4">Total: ${total}</div>
     </div>
   );
 };
